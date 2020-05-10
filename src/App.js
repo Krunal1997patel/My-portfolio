6a72ea9105e319d5e58f9cea70263d964119e8d7
Krunal Patel <Pkrunal616@gmail.com>
2020-05-09 21:00:39 -0400

    adding nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import Nav from './components/Nav';
 import About from './components/About';
 import Project from './components/Project';
 import Skills from './components/Skill';
@@ -8,6 +9,7 @@ import Contact from './components/Contact';
 function App() {
 	return (
 		<div className='App'>
+			<Nav />
 			<Dashboard />
 			<About />
 			<Skills />
