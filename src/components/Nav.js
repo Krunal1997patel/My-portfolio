12ec91d5bb0068ec404dc0ea33c9cd6c38d6897d
Krunal Patel <Pkrunal616@gmail.com>
2020-05-10 21:37:06 -0400

    adding dark mode

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import useDarkMode from '../hooks/useDarkMode';
 
 const Nav = (props) => {
-	return <div></div>;
+	const [DarkMode, setUseDarkMode] = useDarkMode(false);
+
+	const toggleDarkMode = (e) => {
+		e.preventDefault();
+		setUseDarkMode(!DarkMode);
+	};
+
+	return (
+		<div>
+			{DarkMode === true ? (
+				<button
+					onClick={toggleDarkMode}
+					className={DarkMode ? 'dark-button dark-mode' : 'dark-button'}
+				>
+					Light Mode
+				</button>
+			) : (
+				<button
+					onClick={toggleDarkMode}
+					className={DarkMode ? 'dark-button dark-mode' : 'dark-button'}
+				>
+					Dark Mode
+				</button>
+			)}
+		</div>
+	);
 };
 
 export default Nav;
