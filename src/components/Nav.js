680885e6eb49bc46e43f19f2374a61099151c13f
Krunal Patel <Pkrunal616@gmail.com>
2020-05-12 23:49:24 -0400

    new lay out

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,47 +1,51 @@
 import React from 'react';
-import useDarkMode from '../hooks/useDarkMode';
+// import useDarkMode from '../hooks/useDarkMode';
 import '../styling/navbar.scss';
 
 const Nav = (props) => {
-	const [DarkMode, setUseDarkMode] = useDarkMode(false);
+	// const [DarkMode, setUseDarkMode] = useDarkMode(false);
 
-	const toggleDarkMode = (e) => {
-		e.preventDefault();
-		setUseDarkMode(!DarkMode);
-	};
+	// const toggleDarkMode = (e) => {
+	// 	e.preventDefault();
+	// 	setUseDarkMode(!DarkMode);
+	// };
 
 	return (
 		<div className='navbar'>
-			<button className='link' href='#'>
-				<a href='#'>About</a>
-			</button>
-			<button className='link' href='#'>
-				Skill
-			</button>
-			<button className='link' href='#'>
-				Project
-			</button>
-			<button className='link' href='#'>
-				Contact
-			</button>
+			<div className='center'>
+				<div>
+					<a href='#'>Kp</a>
+				</div>
 
-			{DarkMode === true ? (
-				<button
-					onClick={toggleDarkMode}
-					className={DarkMode ? 'dark-button dark-mode' : 'dark-button'}
-				>
-					<img src='https://img.icons8.com/nolan/64/smiling-sun.png' />
-				</button>
-			) : (
-				<button
-					onClick={toggleDarkMode}
-					className={DarkMode ? 'dark-button dark-mode' : 'dark-button'}
-				>
-					<img src='https://img.icons8.com/nolan/64/full-moon.png' />
-				</button>
-			)}
+				<div className='allLinks'>
+					<a href='#'>About</a>
+
+					<a href='#'>Skill</a>
+
+					<a href='#'>Project</a>
+
+					<a href='#'>Contact</a>
+				</div>
+			</div>
 		</div>
 	);
 };
 
 export default Nav;
+
+{
+	/* {DarkMode === true ? (
+	<button
+		onClick={toggleDarkMode}
+		className={DarkMode ? 'dark-button dark-
+		<img src='https://img.icons8.com/nolan/64/smiling-sun.png' />
+	</button>
+) : (
+	<button
+		onClick={toggleDarkMode}
+		className={DarkMode ? 'dark-button dark-mode' : 'dark-button'}
+	>
+		<img src='https://img.icons8.com/nolan/64/full-moon.png' />
+	</button>
+)} */
+}
