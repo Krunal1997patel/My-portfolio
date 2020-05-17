126b335dae49e26b5f3389b12f015ec2bd853bff
Krunal Patel <Pkrunal616@gmail.com>
2020-05-17 01:18:47 -0400

    changed project text

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,6 +12,10 @@ const Project = (props) => {
 			name: 'Celebrities Dead or Alive',
 			info:
 				'This application is a questionaire that tests whether you know if a celebrity is still alive or not. ',
+			built:
+				'Developed using React, Redux, Styled Components, Jest, React Router, and full CRUD operations.',
+			work:
+				'I was in charge of styling the full application and making the sign-up, home, and timer part of the application also worked on the functionality of the main part of the application changing the image after user selecting their answer and pick random images in the database.',
 			demo: 'https://celeb-doa.netlify.app/',
 			github: 'https://github.com/Buildweek-Dead-Celebrities',
 		},
@@ -20,6 +24,10 @@ const Project = (props) => {
 			name: 'Gigapet API',
 			info:
 				'This application was designed to help parents create a diet plan for their children to keep them healthy.',
+			built:
+				'Developed using Node.js, Express, Knex, postgresql, Jest, Cors, and Jsonwebtoken, ',
+			work:
+				' my role in this project was fully developing the backend with RESTful API and deploying the Gigapet backend on Heroku.',
 			demo: 'https://build-gigapet.herokuapp.com',
 			github: 'https://github.com/Build-Gigapet/Back-End',
 		},
@@ -28,6 +36,10 @@ const Project = (props) => {
 			name: 'Rick And Morty',
 			info:
 				'This is a Rick & Morty Fan Page where the fan of the show can search up their favorite character and location.',
+			built:
+				'Developed using React, Hooks, Styled cComponents, React Router, and Axios for rick and morty API',
+			work:
+				' I fully developed this application with the styling of the page and functionality as a user can filter any character or location with the search bar, and searching any character in the random part of the application. ',
 			demo: 'https://rick-info.netlify.app',
 			github:
 				'https://github.com/Krunal1997patel/Sprint-Challenge-Single-Page-Apps/tree/krunal-patel',
@@ -48,7 +60,9 @@ const Project = (props) => {
 								alt={project.name}
 							/>
 							<h3 className='name'>{project.name}</h3>
-							<p className='info'>{project.info}</p>
+							<p className='info'>&bull; {project.info}</p>
+							<p className='info'>&bull; {project.built}</p>
+							<p className='info'>&bull; {project.work}</p>
 							<div className='links'>
 								<a className='demo' href={project.demo} target='blank'>
 									Demo
